304951666f69c1addaa5a973331997421f3819c8
Tero Saarni <tero.saarni@gmail.com>
2024-03-10 17:45:30 +0200

    gettextize with gnu gettext m4 macros

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
+#include <locale.h>
 #include <stdio.h>
 
+#include "config.h"
+#include "gettext.h"
+
+#define _(msgid) dgettext(PACKAGE, msgid)
+
 int main() {
-  printf("Hello, World!\n");
+  setlocale(LC_ALL, "");
+  bindtextdomain(PACKAGE, LOCALEDIR);
+
+  printf("%s", _("Hello, World!\n"));
   return 0;
 }
